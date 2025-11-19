2ec7fa75df3e1bb6f7e28aeefed8539e92c5cabe
KEELANMURPHY-STUDENT <G00435965@atu.ie>
2025-11-19 14:14:44 +0000

    Created packages and vehicle, vehicleservice, and vehicle controller classes.

---
+++ src/main/java/ie/atu/cicd1_exam_19/service/VehicleService.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1_exam_19.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleService {
+    private final List<vehicle> store = new ArrayList<> {
+        for (vehicle v : store) {
+            if (v.getregNumber().equals(id))
+        }
+    }
+}
